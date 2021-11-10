fc1e2c93cb4e5f362dc0f13acda136229c0f819a
ahfaisalkarimi <ahfaisalkarimi@gmail.com>
2021-11-10 10:32:50 -0800

    Initial commit

---
+++ src/components/Heading.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <h3>
+      Enter all of the choices divided by a comma (','). <br /> Press enter when
+      you're done
+    </h3>
+  );
+};
+
+export default Heading;
---
+++ src/components/TextArea.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TextArea = () => {
+  return (
+    <textarea placeholder="Enter choices here..." id="textarea"></textarea>
+  );
+};
+
+export default TextArea;
